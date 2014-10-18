13284711cc0b8ddb14817b8ebb1071271a51f424
Bridget <bridget@Bridgets-MacBook-Pro.local>
2014-10-18 07:10:11 -0400

    awesome first UI

---
+++ thirdtimesthecharm/MainPage.h
@@ -0,0 +1,13 @@
+//
+//  MainPage.h
+//  thirdtimesthecharm
+//
+//  Created by Bridget on 10/18/14.
+//  Copyright (c) 2014 Bridget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainPage : UIViewController
+
+@end
